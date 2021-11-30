ab489e9b55931e5624f1ab4f9b41718573e8cf92
sjcl <bebe@sjcl.me>
2021-12-01 00:40:49 +0900

    Fix whitespace

--- src/components/Mosaic.vue
+++ src/components/Mosaic.vue
@@ -1,13 +1,13 @@
 <template>
   <p v-if="loading">Now Loading...</p>
   <div v-else>
-    <img src="/original.jpg" :class="showOriginal ? 'image trans show' : 'image trans hide'" />
+    <img src="/original.jpg" :class="getOriginalImageClass()" />
     <image-zoom
       zoom="/mosaic.png"
       regular="/thumb.jpg"
       :zoom-amount="7"
       click-zoom
-      img-class="image"
+      :img-class="getImageClass()"
       :class="showOriginal ? 'trans hide' : 'trans show'"
     ></image-zoom>
     <div class="buttons">
@@ -16,19 +16,18 @@
         color="pink lighten-1"
         class="mx-1 white--text"
         large
-      >Switch<br>切り替え</v-btn>
-      <v-btn
-        @click="showMessages = true"
-        color="pink lighten-1"
-        class="mx-1 white--text"
-        large
-      >Message/arts<br>メッセージ/アート</v-btn>
-      <v-btn
-        @click="showCredits = true"
-        color="pink lighten-1"
-        class="mx-1 white--text"
-        large
-      >Credits<br>クレジット</v-btn>
+      >
+        Switch
+        <br />切り替え
+      </v-btn>
+      <v-btn @click="showMessages = true" color="pink lighten-1" class="mx-1 white--text" large>
+        Message/arts
+        <br />メッセージ/アート
+      </v-btn>
+      <v-btn @click="showCredits = true" color="pink lighten-1" class="mx-1 white--text" large>
+        Credits
+        <br />クレジット
+      </v-btn>
     </div>
 
     <v-overlay :value="showMessages" opacity=".6" absolute class="text-center">
@@ -69,22 +68,44 @@ export default {
       loading: true,
       showOriginal: true,
       showMessages: false,
-      showCredits: false
+      showCredits: false,
+      isMobile: window.screen.width < window.screen.height
     };
   },
   mounted() {
+    console.log(this.isMobile);
     window.onload = () => {
       this.loading = false;
       setTimeout(() => {
         this.showOriginal = false;
       }, 2000);
     };
+  },
+  methods: {
+    getImageClass() {
+      if (this.isMobile) return "image-mobile";
+      return "image";
+    },
+    getOriginalImageClass() {
+      if (this.isMobile) {
+        if (this.showOriginal) return "image-mobile trans show";
+        return "image-mobile trans hide";
+      } else {
+        if (this.showOriginal) return "image trans show";
+        return "image trans hide";
+      }
+    }
   }
 };
 </script>
 
 <style>
 .image {
+  width: 100vw;
+  
+}
+
+.image-mobile {
   height: 100vh;
 }
 
