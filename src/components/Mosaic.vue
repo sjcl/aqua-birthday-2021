9f66fc0d8408633f421e7288d48c8a8685cc1fb0
sjcl <bebe@sjcl.me>
2021-11-30 17:09:38 +0900

    Add switch button

--- src/components/Mosaic.vue
+++ src/components/Mosaic.vue
@@ -1,15 +1,16 @@
 <template>
-<div>
-  <img src="/original.jpg" :class="original ? 'image trans show' : 'image trans hide'" />
-  <image-zoom
-    zoom="/mosaic.png"
-    regular="/thumb.jpg"
-    :zoom-amount="7"
-    click-zoom
-    img-class="image"
-    :class="original ? 'trans hide' : 'trans show'"
-  ></image-zoom>
-</div>
+  <div>
+    <img src="/original.jpg" :class="original ? 'image trans show' : 'image trans hide'" />
+    <image-zoom
+      zoom="/mosaic.png"
+      regular="/thumb.jpg"
+      :zoom-amount="7"
+      click-zoom
+      img-class="image"
+      :class="original ? 'trans hide' : 'trans show'"
+    ></image-zoom>
+    <v-btn @click="original = !original" color="pink lighten-1" class="white--text" fixed right bottom x-large>Switch / 切り替え</v-btn>
+  </div>
 </template>
 
 <script>
@@ -26,22 +27,22 @@ export default {
   },
   mounted() {
     const img = new Image();
-    img.onload = () => {
-      console.log("img loaded");
-    };
+    // img.onload = () => {
+    //   console.log("img loaded");
+    // };
     img.src = "/mosaic.png";
 
-    setInterval(() => {
-      this.original = false
-    }, 2000)
+    setTimeout(() => {
+      this.original = false;
+    }, 2000);
   }
 };
 </script>
 
 <style>
 div {
-	font-size: 0;
-	line-height: 0;
+  font-size: 0;
+  line-height: 0;
 }
 
 .image {
