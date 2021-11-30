8babe4ddc29ed489d5624afd6d7efea1e2cc4a33
sjcl <bebe@sjcl.me>
2021-11-30 15:33:55 +0900

    Switch original to mosaic

--- src/components/Mosaic.vue
+++ src/components/Mosaic.vue
@@ -1,11 +1,15 @@
 <template>
+<div>
+  <img :src="require('@/assets/sample.jpg')" :class="original ? 'image trans show' : 'image trans hide'" />
   <image-zoom
     :zoom="require('@/assets/mosaic.png')"
     :regular="require('@/assets/thumb.jpg')"
     :zoom-amount="7"
     click-zoom
-    img-class="mosaic"
+    img-class="image"
+    :class="original ? 'trans hide' : 'trans show'"
   ></image-zoom>
+</div>
 </template>
 
 <script>
@@ -15,18 +19,46 @@ export default {
   components: {
     imageZoom
   },
+  data() {
+    return {
+      original: true
+    };
+  },
   mounted() {
     const img = new Image();
     img.onload = () => {
       console.log("img loaded");
     };
     img.src = require("@/assets/mosaic.png");
+
+    setInterval(() => {
+      this.original = false
+    }, 1000)
   }
 };
 </script>
 
 <style>
-.mosaic {
+div {
+	font-size: 0;
+	line-height: 0;
+}
+
+.image {
   height: 100vh;
+}
+
+.trans {
+  position: absolute;
+}
+
+.show {
+  opacity: 1;
+  transition: opacity 1.5s;
+}
+
+.hide {
+  opacity: 0;
+  transition: opacity 1.5s;
 }
 </style>
