7952c9268f98cd096e6748f58aba6a84a7470421
sjcl <bebe@sjcl.me>
2021-12-01 00:18:58 +0900

    Fix overlay message view

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -3,15 +3,15 @@
     <div class="text-center text-h4 my-1">Messages</div>
     <div class="text-center text-h4 my-1">メッセージ</div>
     <v-row>
-      <v-col v-for="msg in messages" :key="msg" cols="3">
-        <v-img :src="'/messages/'+msg+'.jpg'" />
+      <v-col v-for="msg in messages" :key="msg" cols="12" lg="3" md="6"  sm="12">
+        <v-img :src="'/messages/'+msg+'.jpg'" max-width="100vw" />
       </v-col>
     </v-row>
     <div class="text-center text-h4 my-1">Arts</div>
     <div class="text-center text-h4 my-1">アート</div>
     <v-row>
-      <v-col v-for="img in images" :key="img" cols="3">
-        <v-img :src="'/images/'+img+'.jpg'" />
+      <v-col v-for="img in images" :key="img" cols="12" lg="3" md="6" sm="12">
+        <v-img :src="'/images/'+img+'.jpg'" max-width="100vw"/>
       </v-col>
     </v-row>
   </v-container>
