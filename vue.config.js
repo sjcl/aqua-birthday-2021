60763bdc571b200334425c979d57c6a42fd29ea0
sjcl <bebe@sjcl.me>
2021-11-30 23:33:57 +0900

    Update site name

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   pages: {
     index: {
       entry: "src/main.js",
-      title: "あくたん誕生日記念ファンプロジェクト",
+      title: "あくたん誕生日記念ファンプロジェクト | Aqua Birthday 2021",
     }
   },
   transpileDependencies: [
