551e7eb4e67609be58eded99546e14c556654d63
sjcl <bebe@sjcl.me>
2021-11-30 15:09:49 +0900

    Add basic mosaic component

--- src/components/Mosaic.vue
+++ src/components/Mosaic.vue
@@ -1,12 +1,32 @@
 <template>
-<v-container><zoombox :src="require('@/assets/mosaic.png')" :thumb="require('@/assets/thumb.jpg')"></zoombox></v-container>
-
+  <image-zoom
+    :zoom="require('@/assets/mosaic.png')"
+    :regular="require('@/assets/thumb.jpg')"
+    :zoom-amount="7"
+    click-zoom
+    img-class="mosaic"
+  ></image-zoom>
 </template>
 
 <script>
-    export default {
-        components: {
-            "zoombox": require("vue-zoombox")
-        }
+import imageZoom from "vue-image-zoomer";
+
+export default {
+  components: {
+    imageZoom
+  },
+  mounted() {
+    const img = new Image();
+    img.onload = () => {
+      console.log("img loaded");
     };
+    img.src = require("@/assets/mosaic.png");
+  }
+};
 </script>
+
+<style>
+.mosaic {
+  height: 100vh;
+}
+</style>
