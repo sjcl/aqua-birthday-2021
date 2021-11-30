9eca79599faf5277c134744fe840e62c607963f4
sjcl <bebe@sjcl.me>
2021-11-30 13:30:07 +0900

    Test with vue-zoombox

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,7 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from './components/Mosaic';
 
 export default {
   name: 'App',
---
+++ src/components/Mosaic.vue
@@ -0,0 +1,12 @@
+<template>
+<v-container><zoombox :src="require('@/assets/mosaic.png')" :thumb="require('@/assets/thumb.jpg')"></zoombox></v-container>
+
+</template>
+
+<script>
+    export default {
+        components: {
+            "zoombox": require("vue-zoombox")
+        }
+    };
+</script>
